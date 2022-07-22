b5164b37863c61a98665505192b2aca0e4ebfba5
SAURABH ARYA <42316833+souravarya07@users.noreply.github.com>
2022-07-22 23:45:29 +0530

    Maven project files added
    Spring boot version :  2.7.2
    Maven : 3.8.2
    Java : 1.8

---
+++ BloggingWebsite/BloggingWebsite/src/main/java/com/blog/project/BloggingWebsite/BloggingWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.blog.project.BloggingWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggingWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggingWebsiteApplication.class, args);
+	}
+
+}
