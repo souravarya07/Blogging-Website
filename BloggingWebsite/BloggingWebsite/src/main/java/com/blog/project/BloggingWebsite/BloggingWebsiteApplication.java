66d12d6a0f63cce167d21396421e7be6b1aa3661
SAURABH ARYA <42316833+souravarya07@users.noreply.github.com>
2022-07-23 22:23:57 +0530

    Done changes in SpringBootApplication annotation

--- BloggingWebsite/BloggingWebsite/src/main/java/com/blog/project/BloggingWebsite/BloggingWebsiteApplication.java
+++ BloggingWebsite/BloggingWebsite/src/main/java/com/blog/project/BloggingWebsite/BloggingWebsiteApplication.java
@@ -2,8 +2,9 @@ package com.blog.project.BloggingWebsite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class BloggingWebsiteApplication {
 
 	public static void main(String[] args) {
