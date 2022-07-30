3a0af2be0edfe8915d13ce5ea59a9ba6622f33ea
SAURABH ARYA <42316833+souravarya07@users.noreply.github.com>
2022-07-31 00:55:28 +0530

    Database changes

---
+++ BloggingWebsite/BloggingWebsite/src/main/java/com/blog/project/BloggingWebsite/model/Post.java
@@ -0,0 +1,96 @@
+package com.blog.project.BloggingWebsite.model;
+
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import java.util.Date;
+
+@Entity
+@Table(name = "Post")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    @NotBlank
+    private String title;
+
+    @Lob
+    @Column
+    @NotEmpty
+    private String content;
+
+    @Column
+    @NotEmpty
+    private Date createdOn;
+
+    @Column
+    @NotEmpty
+    private Date updatedOn;
+
+    @Column
+    @NotEmpty
+    private String userName;
+
+//    @Column(name = "photo", columnDefinition = "BLOB")
+//    private byte[] photo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(Date createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public Date getUpdatedOn() {
+        return updatedOn;
+    }
+
+    public void setUpdatedOn(Date updatedOn) {
+        this.updatedOn = updatedOn;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+//    public byte[] getPhoto() {
+//        return photo;
+//    }
+//
+//    public void setPhoto(byte[] photo) {
+//        this.photo = photo;
+//    }
+}
