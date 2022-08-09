3911568529fb41f8d6c30e5c7bdc9457d823c156
SAURABH ARYA <42316833+souravarya07@users.noreply.github.com>
2022-08-10 00:23:47 +0530

    Added Password encoder in security configurations

--- BloggingWebsite/BloggingWebsite/src/main/java/com/blog/project/BloggingWebsite/Service/AuthService.java
+++ BloggingWebsite/BloggingWebsite/src/main/java/com/blog/project/BloggingWebsite/Service/AuthService.java
@@ -4,6 +4,7 @@ import com.blog.project.BloggingWebsite.Repository.UserRepository;
 import com.blog.project.BloggingWebsite.dto.RegisterRequest;
 import com.blog.project.BloggingWebsite.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,11 +13,18 @@ public class AuthService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public void signup(RegisterRequest registerRequest) {
         User user = new User();
         user.setUserName(registerRequest.getUsername());
-        user.setPassword(registerRequest.getPassword());
+        user.setPassword(encodePassword(registerRequest.getPassword()));
         user.setEmail(registerRequest.getEmail());
         userRepository.save(user);
+    }
+
+    private String encodePassword(String password) {
+        return passwordEncoder.encode(password);
     }
 }
